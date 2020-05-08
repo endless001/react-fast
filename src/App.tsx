8ad5066bbfc69e2133c09d2ac0c6c1009023ec3d
longqing <1319822160@qq.com>
2020-05-08 13:33:47 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+
+import SignIn from './views/user/SignIn'
+import SignUp from './views/user/SignUp'
+import Home from './views/home/Home'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/user/sigin">
+          <SignIn />
+        </Route>
+        <Route path="/user/sigup">
+          <SignUp />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
+
 export default App
---
+++ src/views/home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../../components/Layout/Layout'
+
+const Home = () => {
+  return (
+  <Main />
+  )
+}
+export default Home
