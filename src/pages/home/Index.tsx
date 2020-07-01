181a5cd61a66b7ccde48ae6df9b282fdc9cc7ecc
longqing <1319822160@qq.com>
2020-07-01 13:30:13 +0800

    update

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,8 +1,62 @@
-import React from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const Index = () => {
-  return (
-    <div>index</div>
-    )
+
+
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+        border: 0,
+    },
+});
+
+function createData(name:any, calories:any, fat:any, carbs:any, protein:any) {
+    return { name, calories, fat, carbs, protein };
 }
-export default Index
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Customer() {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper} className="table" elevation={0} >
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>名称</TableCell>
+                        <TableCell align="right">所有者</TableCell>
+                        <TableCell align="right">上次修改日期</TableCell>
+                        <TableCell align="right">文件大小</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
